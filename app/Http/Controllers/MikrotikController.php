b69a74993cd07cdcbaadb2336acc887d250e86a9
tony <fathoninur90@gmail.com>
2024-12-15 21:21:38 +0700

    Update MikrotikController.php

--- app/Http/Controllers/MikrotikController.php
+++ app/Http/Controllers/MikrotikController.php
@@ -9,28 +9,21 @@ use RouterOS\Query;
 
 class MikrotikController extends Controller
 {
-    public function showPPPoE()
+    public function showPPPOE()
     {
+        // Konfigurasi koneksi ke Mikrotik
         $client = new Client([
-            'host' => '10.10.10.1',
-            'user' => 'project',
-            'pass' => 'project',
-            'port' => 8728,
+            'host' => '10.10.10.1',  // IP Mikrotik
+            'user' => 'project',     // Username Mikrotik
+            'pass' => 'project',     // Password Mikrotik
+            'port' => 8728,          // Port API Mikrotik
         ]);
 
         try {
             $query = new Query('/ppp/active/print');
             $response = $client->query($query)->read();
 
-            // Format uptime untuk setiap koneksi
-            $formattedResponse = array_map(function ($connection) {
-                $connection['uptime'] = isset($connection['uptime']) 
-                    ? Helper::formatUptime($connection['uptime']) 
-                    : 'N/A';
-                return $connection;
-            }, $response);
-
-            return view('pages.pppoe', ['activePPPoE' => $formattedResponse]);
+            return view('pages.pppoe', ['activePPPoE' => $response]);
         } catch (\Exception $e) {
             return response()->json(['error' => 'Gagal terhubung ke Mikrotik: ' . $e->getMessage()]);
         }
